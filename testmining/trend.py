9d883ddfea145c0655b41e6bf2825bf67b883270
Falco Duersch <falco.duersch@student.hpi.de>
2019-07-19 13:57:44 +0200

    Fix trend module

--- testmining/trend.py
+++ testmining/trend.py
@@ -1,7 +1,8 @@
 # -*- encoding: utf-8 -*-
-import click
 import logging
 import os
+
+import click
 
 import altair as alt
 import pandas as pd
@@ -11,53 +12,64 @@ from testmining import folders
 LOG = logging.getLogger(__file__)
 
 
-@click.group()
-def cli():
-    pass
-
-
-@cli.command()
-def apfd():
+def collect_apfd():
     frames = []
     for project_name, project_path in folders.projects():
-        df = pd.read_csv(folders.apfd(project_path))[['untreated']].reset_index()
-        df['trend'] = df['untreated'].rolling(50, min_periods=1).mean()
-        #df['trend'] = (df['untreated'] / 0.5) * 100
-        df['project'] = project_name
-        frames.append(df)
-
-    df = pd.concat(frames)
+        frames.append(read_trend(project_name, project_path))
+    return pd.concat(frames)
 
 
-    #scatter = alt.Chart().mark_point(filled=True).encode(
-    #    x='index',
-    #    y='untreated',
-    #)
+def read_trend(project_name, project_path):
+    df = pd.read_csv(folders.apfd(project_path))[['untreated']].reset_index()
+    df['trend'] = df['untreated'].rolling(50, min_periods=1).mean()
+    df['project'] = project_name
+    return df
 
-    line = alt.Chart().mark_line(color='red').encode(
+
+def apfd_line():
+    return alt.Chart().mark_line().encode(
         x=alt.X('index'),
         y=alt.Y('trend'),
+        color=alt.Color('project'),
+    )
+
+
+def apfd_scatter():
+    return alt.Chart().mark_point(filled=True).encode(
+        x='index',
+        y='untreated',
         color='project',
     )
 
-    line.properties(data=df).save(os.path.expanduser('~/tmp/schrott.png'))
 
-    #output = os.path.join(folders.evaluation(project_path), 'apfd-trend.png')
-    #alt.layer(scatter, line).properties(data=df, title=project_name).save(output)
-    #LOG.info('Written %s', output)
-
-
-@cli.command()
-@click.option('--output', required=True)
-def tests(output):
+def collect_tests():
+    frames = []
     for project_name, project_path in folders.projects():
-        df = pd.read_csv(folders.strategy(project_path, 'untreated'))
-        df = df[['travisBuildNumber', 'count']]
-        df['count'] = df['count'].rolling(50, min_periods=1).mean()
+        raw = pd.read_csv(folders.strategy(project_path, 'untreated'))
+        df = raw.groupby('travisBuildNumber').agg({'count': 'sum'}).reset_index()
         df['project'] = project_name.split('@')[1]
-        output = os.path.join(folders.evaluation(project_path))
+        df['count'] = df['count'].rolling(50, min_periods=1).mean()
+        frames.append(df)
+    return pd.concat(frames)
+
+
+def tc_line():
+    return alt.Chart().mark_line().encode(
+        x='travisBuildNumber',
+        y='count',
+        color='project',
+    )
+
+
+@click.command()
+def main():
+    for project_name, project_path in folders.projects():
+        df = read_trend(project_name, project_path)
+        chart = apfd_line().properties(data=df)
+        output = os.path.join(folders.evaluation(project_path), 'apfd-trend.png')
+        chart.save(output)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    cli()
+    main()
